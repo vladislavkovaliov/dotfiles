2c8d43bfca10e9be8e41c5f60631c392b203c443
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2026-02-14 23:14:52 +0300

    fix: does use ip route get to get device instad of nmcli

--- waybar/scripts/ip/ip.sh
+++ waybar/scripts/ip/ip.sh
@@ -2,18 +2,15 @@
 
 active_conn_info=$(nmcli -t -f DEVICE,TYPE,STATE dev status | grep ":connected" | head -n1)
 
-if [ -z "$active_conn_info" ]; then
+device=$(ip route get 8.8.8.8 2>/dev/null | awk '{print $5; exit}')
+
+if [ -z "$device" ]; then
   echo "{\"text\":\"No network\",\"class\":\"disconnected\"}"
- 
+  
   exit 0
 fi
 
-device=$(echo "$active_conn_info" | cut -d: -f1)
-
-type=$(echo "$active_conn_info" | cut -d: -f2)
-
-
 ip_addr=$(ip -4 addr show "$device" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
-echo "{\"text\":\"$type: $ip_addr\",\"class\":\"connected\"}"
+echo "{\"text\":\"$device: $ip_addr\",\"class\":\"connected\"}"
 
