44b97572710c5b3d3fc2f5efd4bef9af548941c1
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2026-02-21 13:35:14 +0000

    feat: defines base package for install

---
+++ scripts/pkgs.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+general=(
+	"wget"
+	"curl"
+	"git"
+	"unzip"
+	"vim"
+)
+
+
+hyprland=(
+	"hyprpaper"
+	"hyprlock"
+	"hypridle"
+	"hyprpicket"
+	"xdg-desktop-portal-hyprland"
+)
+
+
+app=(
+	"wlogout"
+	"vlc"
+	"waybar"
+	"rofi"
+	"neovim"
+	"blueman"
+)
+
+
+utils=(
+	"btop"	
+)
